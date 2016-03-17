d6618f10cadaf5de4e04b9add49d8b5ecbb20101
I-Kang Ding <ikding@gmail.com>
2016-03-17 00:17:27 -0400

    Refactor SWE salary analysis for quora, after COLI adj
    https://www.quora.com/What-percentage-of-software-engineers-make-at-least-200-000/answer/I-Kang-Ding

--- bls_salary_analysis.R
+++ bls_salary_analysis.R
@@ -66,14 +66,14 @@ data$metro_state <- regmatches(data$area_name, r)
 coli$metro_short <- paste(coli$metro_first_city, coli$metro_state)
 data$metro_short <- paste(data$metro_first_city, data$metro_state)
 
-# bls_name <- unique(data$metro_short)
-# coli_name <- unique(coli$metro_short)
-# 
-# table(bls_name %in% coli_name)
-# table(coli_name %in% bls_name)
-# 
-# sort(coli_name[!(coli_name %in% bls_name)])
-# sort(bls_name[!(bls_name %in% coli_name)]) ###
+bls_name <- unique(data$metro_short)
+coli_name <- unique(coli$metro_short)
+
+table(bls_name %in% coli_name)
+table(coli_name %in% bls_name)
+
+sort(coli_name[!(coli_name %in% bls_name)])
+sort(bls_name[!(bls_name %in% coli_name)]) ###
 
 # Manual correction of coli city names to get better match
 coli[!(coli$metro_short %in% bls_name), c("metro_short", "metro_micro")]
@@ -85,9 +85,7 @@ coli[with(coli, metro_first_city == "Myrtle Beach"),]$metro_state <- "SC"
 
 coli$metro_short <- paste(coli$metro_first_city, coli$metro_state)
 
-unique(data[grepl("Portland", data$area_name),]$area_name)
-
-# For coli index, keep only the city with the highest composite index (most likely to be core metro)
+# For coli with multiple city in one metro (eg. NYC-Manhattan, NYC-Brooklyn, etc), keep only the city with the highest composite index (most likely to be core metro)
 coli <- coli[!coli$metro_micro == "NonMetro US",]
 coli_city_freq <- as.data.frame(table(coli$metro_short))
 coli_duplicate_city <- subset(coli_city_freq, Freq > 1)$Var1
@@ -104,7 +102,7 @@ coli <- as.data.frame(coli %>%
 # metro_short is our join key
 # Keep only some columns of interest
 
-salary <- merge(select(data, metro_short, prim_state:loc.quotient, a_mean:mean_prse, a_pct10:a_pct90), select(coli, metro_short, metro_micro, composite:misc), by = "metro_short", sort = F)
+salary <- merge(select(data, metro_short, prim_state:loc.quotient, a_mean:mean_prse, a_pct10:a_pct90), select(coli, metro_short, metro_micro, composite:misc), by = "metro_short", all.x = T, sort = F)
 
 salary <- arrange(salary, prim_state, area, occ_code)
 
@@ -119,7 +117,7 @@ for (pct in c(10, 25, 50, 75, 90)){
 # Rename the variables to make them more comprehensible for shiny app
 salary <- select(salary, 
                  primary_city = metro_short, 
-                 metro_name = metro_micro,
+                 metro_name = area_name,
                  state = prim_state, 
                  area,
                  occ_code, 
@@ -148,75 +146,98 @@ salary <- select(salary,
                  coli_transportation = transportation,
                  coli_healthcare = healthcare,
                  coli_misc = misc)
-       
-
-(occ_title, occ_group, tot_emp, jobs_1000, loc.quotient, a_mean, a_pct_50, a_pct_50_adj, compsite)
 
 saveRDS(object = salary, file = "data/salary_coli.RDS")
 
 ########################################
 #### Quick Analysis of SDE salary --####
 ########################################
+# Answer for quora question:
+# https://www.quora.com/What-percentage-of-software-engineers-make-at-least-200-000/
 
 # Find the top metros with most software engineering jobs
 
 # Software engineers are narrowly defined as:
 # "Software Developers, Applications" and "Software Developers, Systems Software" in BLS definition
-unique(data[grep("Software", data$occ_title),]$occ_title)
+unique(salary[grep("Software", salary$occ_job_title),]$occ_job_title)
 
 # Top 7 metro with most total_employment of software engineers are:
 # San Jose, Washington DC, Seattle, New York, Boston, Chicago, San Francisco
-swe_by_metro <- data[grepl("Software", data$occ_title), c("area_name", "tot_emp")] %>%
-    group_by(area_name) %>%
-    summarise(sum_emp = sum(tot_emp)) %>%
-    arrange(desc(sum_emp))
+salary_swe <- salary[grepl("Software", salary$occ_job_title), c("metro_name", "total_employment")] %>%
+    group_by(metro_name) %>%
+    summarise(sum_employment = sum(total_employment)) %>%
+    arrange(desc(sum_employment))
 
-metro_names <- swe_by_metro$area_name[1:7]
+metro_names <- salary_swe$metro_name[1:7]
 metro_names
 
 # Keep only swe-salary data in these 7 metros 
-data <- subset(data, area_name %in% metro_names)
-data <- data[grepl("Software", data$occ_title),]
+salary_swe <- subset(salary, metro_name %in% metro_names)
+salary_swe <- salary_swe[grepl("Software", salary_swe$occ_job_title) & !is.na(salary_swe$wage_mean),]
 
 # Compute weighted mean of the salary in each quantile
-data_dist <- as.data.frame(data %>%
-    group_by(area_name) %>%
-    summarise(total_emp = sum(tot_emp),
-              pct10 = weighted.mean(a_pct10, tot_emp, na.rm = T),
-              pct25 = weighted.mean(a_pct25, tot_emp, na.rm = T),
-              pct50 = weighted.mean(a_median, tot_emp, na.rm = T),
-              pct75 = weighted.mean(a_pct75, tot_emp, na.rm = T),
-              pct90 = weighted.mean(a_pct90, tot_emp, na.rm = T)))
+salary_dist <- as.data.frame(salary_swe %>%
+    group_by(metro_name) %>%
+    summarise(total_emp = sum(total_employment),
+              pct10 = weighted.mean(wage_pct10, total_employment, na.rm = T),
+              pct25 = weighted.mean(wage_pct25, total_employment, na.rm = T),
+              pct50 = weighted.mean(wage_median, total_employment, na.rm = T),
+              pct75 = weighted.mean(wage_pct75, total_employment, na.rm = T),
+              pct90 = weighted.mean(wage_pct90, total_employment, na.rm = T),
+              pct10_adj = weighted.mean(wage_pct10_adj, total_employment, na.rm = T),
+              pct25_adj = weighted.mean(wage_pct25_adj, total_employment, na.rm = T),
+              pct50_adj = weighted.mean(wage_median_adj, total_employment, na.rm = T),
+              pct75_adj = weighted.mean(wage_pct75_adj, total_employment, na.rm = T),
+              pct90_adj = weighted.mean(wage_pct90_adj, total_employment, na.rm = T)
+              ))
 
-data_dist$area_abb <- sapply(strsplit(data_dist$area_name, "-"), "[[", 1)
+salary_dist$metro_abb <- sapply(strsplit(salary_dist$metro_name, "-"), "[[", 1)
 
 # Calculate mean and stdev with 25% and 75% quantiles (assuming normal distribution)
 
-data_dist
-data_dist$mu <- with(data_dist, (pct25 + pct75)/2)
-data_dist$sigma <- with(data_dist, (pct75-mu)/qnorm(0.75))
+salary_dist$mu <- with(salary_dist, (pct25 + pct75)/2)
+salary_dist$sigma <- with(salary_dist, (pct75-mu)/qnorm(0.75))
+
+salary_dist$mu_adj <- with(salary_dist, (pct25_adj + pct75_adj)/2)
+salary_dist$sigma_adj <- with(salary_dist, (pct75_adj-mu_adj)/qnorm(0.75))
 
 # How much does a 1% engineer make (base salary)?
-data_dist$pct99 <- qnorm(0.99, mean = data_dist$mu, sd = data_dist$sigma)
+salary_dist$pct99 <- qnorm(0.99, mean = salary_dist$mu, sd = salary_dist$sigma)
 
 # What fraction of people makes over 200k a year? How about 150k?
-data_dist$frac.200k <- pnorm(200000, mean = data_dist$mu, sd = data_dist$sigma, lower.tail = F)
-data_dist$frac.150k <- pnorm(150000, mean = data_dist$mu, sd = data_dist$sigma, lower.tail = F)
+salary_dist$frac.150k <- pnorm(150000, mean = salary_dist$mu, sd = salary_dist$sigma, lower.tail = F)
+salary_dist$frac.200k <- pnorm(200000, mean = salary_dist$mu, sd = salary_dist$sigma, lower.tail = F)
 
-# Plot the distributions
-data_plot <- data.frame(x = seq(0, 300000, 1000))
-for (metro in data_dist$area_abb){
-    m = subset(data_dist, area_abb == metro)$mu
-    s = subset(data_dist, area_abb == metro)$sigma
-    data_plot[, metro] <- dnorm(data_plot$x, mean = m, sd = s)
+# Plot the distributions (unadjusted numbers)
+salary_plot <- data.frame(x = seq(0, 300000, 1000))
+for (metro in salary_dist$metro_abb){
+    m = subset(salary_dist, metro_abb == metro)$mu
+    s = subset(salary_dist, metro_abb == metro)$sigma
+    salary_plot[, metro] <- dnorm(salary_plot$x, mean = m, sd = s)
 }
 
-# Convert to long format for ggplot2
-data_plot <- melt(data_plot, id.vars = c("x"))
+salary_plot_adj <- data.frame(x = seq(0, 175000, 1000))
+for (metro in salary_dist$metro_abb){
+    m_adj = subset(salary_dist, metro_abb == metro)$mu_adj
+    s_adj = subset(salary_dist, metro_abb == metro)$sigma_adj
+    salary_plot_adj[, metro] <- dnorm(salary_plot_adj$x, mean = m_adj, sd = s_adj)
+}
 
-ggplot(data = data_plot) +
+
+# Convert to long format for ggplot2
+salary_plot <- melt(salary_plot, id.vars = c("x"))
+salary_plot_adj <- melt(salary_plot_adj, id.vars = c("x"))
+
+ggplot(data = salary_plot) +
     geom_line(aes(x = x, y = value, color = variable), size = 1.5) + 
     scale_color_manual("Metro", values = brewer.pal(8, "Set1")[c(1:5, 7:8)]) + 
     scale_x_continuous("Annual Salary", labels=dollar) + 
+    scale_y_continuous("Probability Density (A.U.)") + 
+    theme(axis.text.y = element_blank())
+
+ggplot(data = salary_plot_adj) +
+    geom_line(aes(x = x, y = value, color = variable), size = 1.5) + 
+    scale_color_manual("Metro", values = brewer.pal(8, "Set1")[c(1:5, 7:8)]) + 
+    scale_x_continuous("Annual Salary (after COLI adjustment)", labels=dollar) + 
     scale_y_continuous("Probability Density (A.U.)") + 
     theme(axis.text.y = element_blank())
