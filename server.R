92f0d4e78ed93597343254c5ee1d91a4c1757888
I-Kang Ding <ikding@gmail.com>
2016-03-21 21:43:02 -0400

    Add download csv button + docstring

--- server.R
+++ server.R
@@ -1,19 +1,18 @@
+# Load libraries
 library(shiny)
 library(rCharts)
 library(BH) # needed for dplyr - load it here so shiny knows to build it during deployment
 library(dplyr)
 
+# Load data that only needs to be loaded when app starts
 salary <- readRDS("data/salary_coli.RDS")
 job_names <- unique(salary$occ_job_title)
 
 shinyServer(function(input, output, session) {
     
+    # Salary data that is reactive to the job title and variables selected
     salary_data <- reactive({
         salary[salary$occ_job_title == input$select_job, input$show_vars, drop = F]
-    })
-    
-    job_names_subset <- reactive({
-        job_names[grepl(pattern = input$job_search_string, x = job_names, ignore.case = T)]
     })
     
     # a large table, reative to input$show_vars
@@ -21,11 +20,17 @@ shinyServer(function(input, output, session) {
         salary_data()
     }, options = list(orderClasses = TRUE))
     
+    # Subset of job titles from the text search - will be used in UI next
+    job_names_subset <- reactive({
+        job_names[grepl(pattern = input$job_search_string, x = job_names, ignore.case = T)]
+    })
+    
     output$job_selector <- renderUI({
         selectInput("select_job", label = h3("Select job"), 
                     as.list(job_names_subset()))
     })
     
+    # Snippet of html text to show the "best" metro on four different metrics
     output$text_metro_insight <- renderUI({
         text1 <- paste("<b>Highest total employment: </b>", 
                        arrange(salary_data(), desc(total_employment))[, "metro_name"][1])
@@ -38,33 +43,14 @@ shinyServer(function(input, output, session) {
         HTML(paste(text1, text2, text3, text4, sep = '<br/><br/>'))
     })
     
-#     output$text_tot_emp <- renderUI({
-#         paste("<b>Highest total employment: </b>", 
-#               arrange(salary_data(), desc(total_employment))[, "metro_name"][1])
-#     })
-#     
-#     output$text_job_1000 <- renderText({
-#         paste("Highest number of", input$select_job, "employment per 1000 overall jobs:",
-#               arrange(salary_data(), desc(job_per_1000))[, "metro_name"][1])
-#     })
-# 
-#     output$text_wage_median <- renderText({
-#         paste("Highest median wage:",
-#               arrange(salary_data(), desc(wage_median))[, "metro_name"][1])
-#     })
-#     
-#     output$text_wage_median_adj <- renderText({
-#         paste("Highest median wage (after cost-of-living adj.):",
-#               arrange(salary_data(), desc(wage_median_adj))[, "metro_name"][1])
-#     })
-    
+    # For second panel: the selected job title
     output$text_job_title <- renderUI({
         job_title <- paste("<h3>", input$select_job, "</h3>")
         HTML(paste(" ", job_title, " ", sep = "</br>"))
     })
     
-    output$chart_var_select <- renderUI({
-        
+    # For second panel: the variable used in bar chart
+    output$chart_var_select <- renderUI({    
         selectInput("select_var", label = "Bar chart variable", 
                     c("Total employment" = "total_employment",
                       "Employment per 1000 over all jobs" = "job_per_1000",
@@ -73,6 +59,7 @@ shinyServer(function(input, output, session) {
         )
     })
     
+    # Interactive barchart with rCharts
     output$barchart1 <- renderChart2({
         salary_subset = salary_data()[order(-salary_data()[, input$select_var]), ]
         salary_subset = salary_subset[c(1:input$top_n),]
@@ -80,11 +67,20 @@ shinyServer(function(input, output, session) {
         p <- nPlot(x = "metro_name", y = input$select_var,
               data = salary_subset,
               type = 'multiBarChart')
-#         p$params$width <- 500
-#         p$params$height <- 800
+        
+        # Format tweaking to accommodate long x-axis labels (metro names)
         p$chart(reduceXTicks = FALSE)
         p$xAxis(staggerLabels = TRUE)
         
         return(p)
     })
+
+    output$downloadData <- downloadHandler(
+        filename = function() {paste0("salary_data", 
+#                                       gsub("[,]?[-\ /][-]*", "_", input$select_job), 
+                                      '.csv')},
+        content = function(file) {
+            write.csv(salary_data(), file, row.names = F)
+    })
+
 })
--- ui.R
+++ ui.R
@@ -6,26 +6,27 @@ var_names_subset <- c("metro_name", "occ_job_title", "total_employment", "job_pe
 
 shinyUI(pageWithSidebar(
     headerPanel("Where Should You Work?"),
+    
     sidebarPanel(
         textInput("job_search_string", label = h3("Search Job Title"),
                   value = "*" ),
         helpText("Case insensitive. Use '*' character to select all job titles. Accepts Regular Expressions"),
         
+        downloadButton('downloadData', 'Download Salary Table'),
+        
         uiOutput("job_selector"),
-        
         htmlOutput("text_metro_insight"),
-#         textOutput("text_tot_emp"),
-#         textOutput("text_job_1000"),
-#         textOutput("text_wage_median"),
-#         textOutput("text_wage_median_adj"),
         
-        checkboxGroupInput('show_vars', label = h3('Columns in data to show:'), names(salary),
+        checkboxGroupInput('show_vars', 
+                           label = h3('Columns in data to show:'), 
+                           names(salary),
                            selected = var_names_subset),
         
         helpText("Data Source:"),
         helpText("Salary: Bureau of Labor Statistics Salary Survey (May 2014)"),
         helpText("Cost-of-Living: C2ER-COLI 2014 Annual Average")
   ),
+  
   mainPanel(
     tabsetPanel(
       tabPanel('Salary Data Table',
@@ -36,7 +37,6 @@ shinyUI(pageWithSidebar(
                numericInput("top_n", "Top N Metros:", 7,
                             min = 1, max = 20),
                showOutput("barchart1", "nvd3")
-#                dataTableOutput("barchart1")
               )
         )
   )
